e8c5770f4535133a0264d19afd63ca09e26c36e6
Timtimtimchen <tim301065@gmail.com>
2023-11-24 01:46:48 +0800

    2

---
+++ index.py
@@ -0,0 +1,27 @@
+import requests
+import bs4
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    html = requests.get('https://www1.pu.edu.tw/~tcyang/course.html')
+    html.encoding = "utf-8"
+    a = bs4.BeautifulSoup(html.text,"html.parser")
+
+    title = a.find("section",class_ = "testimonial-area")
+    titles = title.find_all("div",class_= "team-box")
+
+    # print(title)
+    l = []
+    for i in titles:
+        print(i.h4.text)
+        print(i.p.text)
+        print(i.a["href"])
+        print("https://www1.pu.edu.tw/~tcyang/"+i.a.img["src"])
+        a = [i.h4.text,i.p.text,i.a["href"],"https://www1.pu.edu.tw/~tcyang/"+i.a.img["src"]]
+        l.append(a)
+    print(l)
+    return l
